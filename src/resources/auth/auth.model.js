ba5a021f553e1ab672b43f26c1d1a35956403f1d
hardz <hardzeichyksiarhei@gmail.com>
2020-09-03 01:37:47 +0300

    feat: response role

--- src/resources/auth/auth.model.js
+++ src/resources/auth/auth.model.js
@@ -1,7 +1,7 @@
 class AuthUser {
     static toResponse(user) {
-        const { id, login, avatar_url, email } = user;
-        return { id, login, avatar_url, email, role: 'user' };
+        const { id, login, avatar_url, email, role } = user;
+        return { id, login, avatar_url, email, role };
     }
 }
 
