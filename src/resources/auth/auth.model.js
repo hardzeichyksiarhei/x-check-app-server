a2a260a3cceb2fde440bc999bc7905f3864ebafa
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2020-09-17 02:30:18 +0300

    fix: replace githubId -> id

--- src/resources/auth/auth.model.js
+++ src/resources/auth/auth.model.js
@@ -1,7 +1,7 @@
 class AuthUser {
   static toResponse(user) {
-    const { id: githubId, login, avatar_url, email, role } = user;
-    return { githubId, login, avatar_url, email, role };
+    const { id, login, avatar_url, email, role } = user;
+    return { id, login, avatar_url, email, role };
   }
 }
 
