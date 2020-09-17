736a9086bd3a5e7913a1527ffc02845228a64bbd
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2020-09-17 03:15:51 +0300

    feat: export tasks with checklist format

--- src/resources/tasks/task.controller.js
+++ src/resources/tasks/task.controller.js
@@ -3,18 +3,44 @@ const fetch = require("node-fetch");
 const catchErrors = require("../../common/catchErrors");
 
 exports.exportAll = catchErrors(async (req, res) => {
+  let response;
+
   res.writeHead(200, {
     "Content-Type": "application/json-my-attachment",
     "content-disposition":
       'attachment; filename="export-tasks-' +
-      dateFormat(new Date(), "yyyy-mm-dd-hh-mm-ss") +
+      dateFormat(new Date(), "yyyy-mm-dd_hh-MM-ss") +
       '.json"',
   });
 
-  const response = await fetch(
-    `https://x-check-app-rest-server.herokuapp.com/tasks`
-  );
+  response = await fetch(`https://x-check-app-rest-server.herokuapp.com/tasks`);
   const tasks = await response.json();
 
-  res.end(JSON.stringify(tasks));
+  response = await fetch(
+    `https://x-check-app-rest-server.herokuapp.com/categories`
+  );
+  const categories = await response.json();
+
+  const result = tasks.map((task) => ({
+    taskName: task.title,
+    criteria: categories
+      .filter((category) => category.taskId == task.id)
+      .map((category) =>
+        [
+          {
+            type: "title",
+            title: category.title,
+          },
+        ].concat(
+          category.items.map((item) => ({
+            type: Number(item.score) > 0 ? "subtitle" : "penalty",
+            text: item.description,
+            max: Number(item.score),
+          }))
+        )
+      )
+      .flat(),
+  }));
+
+  res.end(JSON.stringify(result));
 });
